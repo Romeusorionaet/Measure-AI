20e9fd2fcdbced64623decbff640e4c235bc424b
Romeu Sorionaet <romeuindexjs@gmail.com>
2024-10-02 17:53:52 -0300

    refactor: created readme / removed some envs. / defined some envs in compose

---
+++ README.md
@@ -0,0 +1,9 @@
+## Passos para funcionamento da API
+
+1- Depois de clonar o repo, configure seu arquivo .env definindo a env GEMINI_API_KEY=
+
+2- npm install
+
+3- npm run build
+
+4- docker compose up
--- src/infra/http/schemas/env-schema.ts
+++ src/infra/http/schemas/env-schema.ts
@@ -5,10 +5,4 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   GEMINI_API_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333),
-  DB_HOST: z.string(),
-  DB_PORT: z.string(),
-  DB_USER: z.string(),
-  DB_PASSWORD: z.string(),
-  DB_NAME: z.string(),
-  WAIT_HOSTS: z.string(),
 });
